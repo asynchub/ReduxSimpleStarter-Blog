5ed06d46a85738d3dab13d94115f2fc85b86127b
Juri <juri.l@me.com>
2017-08-30 17:33:00 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import ReducerPosts from './reducer_posts';
+// import reducer from redux-form and alias this to reduxForm
+import { reducer as reduxForm } from 'redux-form';
 
+// then add it as form property to object argument of combineReducer function:
 const rootReducer = combineReducers({
-  posts: ReducerPosts
+  posts: ReducerPosts,
+  form: reduxForm
 });
 
 export default rootReducer;
