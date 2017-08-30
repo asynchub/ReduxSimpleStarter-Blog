8283e66b1a7e47efcb23d8122b77f91d07fe192f
Juri <juri.l@me.com>
2017-08-30 17:38:41 +0300

    Update posts_index.js

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-redux-dom';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   const posts = {}; 
@@ -22,6 +24,9 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <Link className="btn btn-primary" to="/posts/new">
+          Add Post
+        </Link>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts}
