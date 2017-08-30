8fe41b5904b27f211b374f205bb7316023451cfb
Juri <juri.l@me.com>
2017-08-30 10:05:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends Component {
+  render { return <div>hello</div> }
+}
+class Bye extends Component {
+  render { return <div>bye</div> }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
