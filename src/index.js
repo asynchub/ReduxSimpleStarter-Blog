136edd30ad281906d4444d40949c38e6f40c5925
Juri <juri.l@me.com>
2017-08-30 17:02:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import promise from redux-promise and pass promise to applyMidleware call.
 import { promise } from 'redux-promise';
 
@@ -11,6 +11,7 @@ import { promise } from 'redux-promise';
 // import App from './components/app';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 // pass promise here to applyMiddleware call:
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -26,7 +27,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
