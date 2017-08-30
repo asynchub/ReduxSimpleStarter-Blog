c1a4ad2a5bac8792fd453c42c2ee6b6a57f246fc
Juri <juri.l@me.com>
2017-08-30 13:13:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+// import promise from redux-promise and pass promise to applyMidleware call.
+import { promise } from 'redux-promise';
 
-import App from './components/app';
+// with BrowseRouter, here App is no longer rendering all components, so:
+// delete App import statement and delete app.js component
+// import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// pass promise here to applyMiddleware call:
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 class Hello extends Component {
   render { return <div>hello</div> }
@@ -20,9 +26,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header
-        <Route path="hi" component={Hello} />
-        <Route path="byyye" component={Bye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
