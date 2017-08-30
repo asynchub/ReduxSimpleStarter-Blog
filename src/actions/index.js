af76c7dbdc9f81112e910029befec58f25f14756
Juri <juri.l@me.com>
2017-08-30 13:09:36 +0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-const ROOT_URL = 'rooturl';
+const ROOT_URL = 'rooturl/api';
+const API_KEY = '?key=....';
 
 export function fetchPosts() {
   
-  const url = `${ROOT_URL}...`
+  const url = `${ROOT_URL}/posts{API_KEY}`
   const request = axios.get(url);
   
   return (
-    type: FETCH_POSTS;
-    payload: 
+    type: FETCH_POSTS,
+    payload: request
   );
 }
