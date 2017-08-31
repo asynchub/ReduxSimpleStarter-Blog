38b5607bedc1a8e8c1f463259c4aedca58127033
Juri <juri.l@me.com>
2017-08-31 12:22:52 +0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 // api has been setup to store blogposts
 // going to make ajax requests to api and 
 // return them by action creator fetchPosts as a promise in variable request.
+// api has routes configured to fetch, create or delete posts
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=FETCH7777';
