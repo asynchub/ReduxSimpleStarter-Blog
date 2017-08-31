ed40d0854c10fda8324ce155296ead30ca8d5943
Juri <juri.l@me.com>
2017-08-31 11:45:53 +0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-// api has been setup to store the list of blog posts and blogposts
-// going to make ajax requests to api and store them as a promise in variable request.
+// api has been setup to store blogposts
+// going to make ajax requests to api and 
+// return them by action creator fetchPosts as a promise in variable request.
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=FETCH7777';
