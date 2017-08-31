5092ace8aee72ef43eca6f2d53d58500665dfcc0
Juri <juri.l@me.com>
2017-08-31 07:40:48 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -28,3 +28,21 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
+
+// how React Router works
+// it has hystory package
+// user changes url by clicking on some link, for example,
+// then browser says to the hystory library: user changed url and here is the new one;
+// hystory takes url, parses or makes some changes to it and passes url to React react router library
+// so, hystory is communicating new route over to react router.
+// React router is receiving new route and decides what set of components to be displayed on the screen, based
+// on exactly what that new route is;
+// our place is to setup the configuration inside router, to configure what components to display for each route
+// once react router has decided, which components to display, it says to react:
+// react, here is the new set of components to display and react router passes them to react to rener them
+// ract takes components and renders them to the screen as content
+// and react waits for new events from user
+// this is idea of SPA, where changing url no longer sends new html docs,
+// SPA idea has no navigation between distinct html docs, instead:
+// we are always using the same html doc and relying on JS to change the set of components, that user sees,
+// appearing on the screen.
