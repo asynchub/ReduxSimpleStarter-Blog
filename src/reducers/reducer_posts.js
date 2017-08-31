6ad09be4159f91ef615cc0c34b4cdbe8f919e87b
Juri <juri.l@me.com>
2017-08-31 12:32:20 +0300

    Update reducer_posts.js

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,9 +4,7 @@ import _ from 'lodash';
 export default function ReducerPosts(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-
-      const posts = _.mapKeys(action.payload.data, 'id');
-      return posts;
+      return _.mapKeys(action.payload.data, 'id');
     default:
       console.log('in reducer');
       return state;
