e8b40e8d866d6079eaccbc43d8daf67a0b7e68e7
Juri <juri.l@me.com>
2017-09-02 20:05:52 +0300

    Update posts_show.js

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchAPost } from '../actions.index';
+import { Link } from 'redux-router-dom';
 
 
 class PostsShow extends Component {
@@ -8,7 +9,7 @@ class PostsShow extends Component {
   componentDidMount() {
     // this is provided to us by react-router:
     // match is the top level poperty from react-router
-    // params is an object that lists all the different tokens,
+    // params is an object that lists all the wildcard tokens,
     // that exist inside the url.
     // id is one of our wildcards from the url
     const { id } = this.props.match.params; // destructuring is used
@@ -16,10 +17,19 @@ class PostsShow extends Component {
   }
 
   render() {
-    this.posts[this.props.match.params.id];
+    const { post } = this.props;
+    
+    if (!post) {
+      return <div>Loading...</div>
+    }
+    
     return (
+      <Link to="/">Back to posts<Link>
+      
       <div>
-        PostsShow
+        <h3>Title: {post.title}</h3>
+        <h6>Category: {post.categories}</h6>
+        <p>{post.text}</p>
       </div>
     );
   }
@@ -29,9 +39,10 @@ class PostsShow extends Component {
 // this component.
 // state argument here is the big lis of posts.
 // with destructuring {} we make it as a variable posts
-// use ownProps, that is cool to get the state own props
+// use ownProps, that is cool to access props of this component
 function mapStateToProps({ posts }, ownProps) {
-  return { posts: [ownProps.match.params.id] };
+  // this declares post and assigns it to what is in [] and returns it
+  return { post: [ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, {fetchAPost})(PostsShow);
